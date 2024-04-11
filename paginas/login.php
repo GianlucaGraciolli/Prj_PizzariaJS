de9d62c2efcf64896f43eb1f48fddaa8993ba9c8
Gianluca <gianluca.graciolli@hotmail.com>
2024-04-10 22:31:22 -0300

    Adicionando Atualizações ao modal

---
+++ paginas/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - JS Pizzaria</title>
+    <link rel="stylesheet" href="./../css/reset.css">
+    <link rel="stylesheet" href="./../css/vars.css">
+    <link rel="stylesheet" href="./../css/style.css">
+    <link rel="stylesheet" href="./../css/login.css">
+</head>
+
+<body>
+    <header class="topo">
+        <h1 class="topo__marca">JS Pizzas <span class="topo__titulo">Menu</span> </h1>
+        <button class="topo__titulo topo__cart-icon">CART</button>
+    </header>
+    <?php
+    echo "Hello World";
+        ?>
+
+</body>
